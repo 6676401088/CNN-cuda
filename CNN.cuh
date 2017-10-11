7e41789e820fad2bcee63381c0ae45d082f62a7f
paperrune <paperrune@naver.com>
2017-10-12 00:05:30 +0900

    Update CNN.cuh

--- CNN.cuh
+++ CNN.cuh
@@ -1,37 +1,41 @@
 #include "cuda_runtime.h"
 
-__global__ void Activate(int option, int number_neuron, float neuron[]);
+__global__ void Activate(int option, int number_neurons, float neuron[]);
 __global__ void Add(int number_memory, float A[], float B[], float C[]);
-__global__ void Adjust_Parameter(int batch_size, int lower_layer_index, int layer_index, int length_filter, int stride, int length_map[], int number_map[], float derivative[], float lower_neuron[], float weight[]);
-__global__ void Backpropagate(int option, int batch_size, int layer_index, int upper_layer_index, int length_upper_filter, int upper_stride, int length_map[], int number_map[], float derivative[], float upper_derivative[], float upper_weight[]);
-__global__ void Batch_Normalization_Activate(int option, int batch_size, int length_map, int number_map, float epsilon, float gamma[], float beta[], float mean[], float variance[], float sum_mean[], float sum_variance[], float neuron[], float neuron_batch_0[], float neuron_batch_1[]);
-__global__ void Batch_Normalization_Adjust_Parameter(int batch_size, int length_map, int number_map, float gamma[], float beta[], float derivative_batch_1[], float neuron_batch_0[]);
-__global__ void Batch_Normalization_Differentiate(int batch_size, int length_map, int number_map, float epsilon, float gamma[], float beta[], float mean[], float variance[], float derivative[], float derivative_batch_0[], float derivative_batch_1[], float neuron_batch_1[]);
-__global__ void Calculate_Loss(int option, int number_neuron, float *error, float neuron[], float target_output[]);
+__global__ void Adjust_Parameter(int batch_size, int lower_layer_index, int layer_index, int kernel_width, int kernel_height, int stride_width, int stride_height, int map_width[], int map_height[], int number_maps[], float derivative[], float lower_neuron[], float weight[]);
+__global__ void Backpropagate(int option, int batch_size, int layer_index, int upper_layer_index, int upper_kernel_width, int upper_kernel_height, int upper_stride_width, int upper_stride_height, int map_width[], int map_height[], int number_maps[], float derivative[], float upper_derivative[], float upper_weight[]);
+__global__ void Batch_Normalization_Activate(int option, int batch_size, int map_width, int map_height, int number_maps, float epsilon, float gamma[], float beta[], float mean[], float variance[], float sum_mean[], float sum_variance[], float neuron[], float neuron_batch_0[], float neuron_batch_1[]);
+__global__ void Batch_Normalization_Adjust_Parameter(int batch_size, int map_width, int map_height, int number_maps, float gamma[], float beta[], float derivative_batch_1[], float neuron_batch_0[]);
+__global__ void Batch_Normalization_Differentiate(int batch_size, int map_width, int map_height, int number_maps, float epsilon, float gamma[], float beta[], float mean[], float variance[], float derivative[], float derivative_batch_0[], float derivative_batch_1[], float neuron_batch_1[]);
+__global__ void Calculate_Loss(int option, int number_neurons, float *error, float neuron[], float target_output[]);
 __global__ void Differentiate(int option, int number_memory, float learning_rate, float derivative[], float neuron[], float target_output[]);
-__global__ void Dropout(int option, int batch_size, int number_map, int length_map, int seed, float rate, float neuron[]);
-__global__ void Feedforward(int option, int batch_size, int lower_layer_index, int layer_index, int length_filter, int stride, int number_map[], int length_map[], float lower_neuron[], float neuron[], float weight[]);
+__global__ void Dropout(int option, int batch_size, int number_maps, int map_width, int map_height, int seed, float rate, float neuron[]);
+__global__ void Feedforward(int option, int batch_size, int lower_layer_index, int layer_index, int kernel_width, int kernel_height, int stride_width, int stride_height, int number_maps[], int map_width[], int map_height[], float lower_neuron[], float neuron[], float weight[]);
 __global__ void Multiply(int number_memory, float A[], float value, float B[]);
 __global__ void Multiply(int number_memory, float A[], float B[], float C[]);
 __global__ void Randomize(int number_memory, int seed, float scale, float shift, float A[]);
 __global__ void Set(int number_memory, float value, float A[]);
-__global__ void Softmax(int batch_size, int length_map, int number_map, float neuron[]);
+__global__ void Softmax(int batch_size, int map_width, int map_height, int number_maps, float neuron[]);
 
 class Convolutional_Neural_Networks_CUDA{
 private:
 	char **type_layer;
 	
 	int batch_size;
-	int number_layer;
-	int number_memory_type;
-	int number_parameter_type;
+	int number_layers;
+	int number_memory_types;
+	int number_parameter_types;
 
-	int *length_filter;
-	int *length_map;
-	int *length_map_factor;
-	int *number_map;
-	int *number_map_factor;
-	int *stride;
+	int *kernel_width;
+	int *kernel_height;
+	int *map_width;
+	int *map_width_factor;
+	int *map_height;
+	int *map_height_factor;
+	int *number_maps;
+	int *number_maps_factor;
+	int *stride_width;
+	int *stride_height;
 
 	float ***derivative;
 	float ***neuron;
@@ -49,8 +53,10 @@ private:
 	// *********************************
 
 	// Variables for Residual Learning
-	int *shortcut_length_filter;
-	int *shortcut_stride;
+	int *shortcut_kernel_width;
+	int *shortcut_kernel_height;
+	int *shortcut_stride_width;
+	int *shortcut_stride_height;
 	// *******************************
 	
 	void Activate(char option[], int layer_index);
@@ -68,7 +74,7 @@ private:
 	bool Access_Memory(int type_index, int layer_index);
 	bool Access_Parameter(int type_index, int layer_index);
 public:
-	Convolutional_Neural_Networks_CUDA(char **type_layer, int number_layer, int length_map[], int number_map[]);
+	Convolutional_Neural_Networks_CUDA(char **type_layer, int number_layers, int map_width[], int map_height[], int number_maps[]);
 	~Convolutional_Neural_Networks_CUDA();
 
 	void Initialize_Parameter(int seed, double scale, double shift);
